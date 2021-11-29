a243db62d3366b5e09c4700e36d8633ae8de4676
MatejTheLes <matej.leskovsek.ce@gmail.com>
2021-11-29 07:47:13 +0100

    connecting microservices

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def hello_world():
 @app.route('/login', methods = ['POST'])
 def login():
     login_data = request.form
-    url = '34.96.72.77/login'
+    url = '34.120.106.247/login'
     response = requests.post(url, data=login_data)
     return response.text
