bf64f81a22456c7d73576dc63bf782c837d8848f
MatejTheLes <matej.leskovsek.ce@gmail.com>
2021-11-29 07:45:05 +0100

    connecting microservices

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 from flask import Flask
 from flask import request
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World 123"
-
-@app.route('/users/<user_id>', methods = ['GET', 'POST', 'DELETE'])
-def user(user_id):
-    if request.method == 'GET':
-        return(user_id)
-    if request.method == 'POST':
-        """modify/update the information for <user_id>"""
-        # you can use <user_id>, which is a str but could
-        # changed to be int or whatever you want, along
-        # with your lxml knowledge to make the required
-        # changes
-        data = request.form # a multidict containing POST data
-        return(data)
-    if request.method == 'DELETE':
-        """delete user with ID <user_id>"""
-        return("User deleted")
-    else:
-        # POST Error 405 Method Not Allowed
-        return("Error 405: Method Not Allowed")
+    return "Login microservice."
     
 @app.route('/login', methods = ['POST'])
 def login():
-    data = request.form
-    if(data["username"]):
-        if(data["username"] == 'matej'):
-            return "200 OK"
-    return(data) 
+    login_data = request.form
+    url = '34.96.72.77/login'
+    response = requests.post(url, data=login_data)
+    return response.text
