1d3b468f7c8355a23925f33a7d0060d669403ca0
MatejTheLes <matej.leskovsek.ce@gmail.com>
2021-12-22 10:14:34 +0100

    egress test

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import requests
 
 app = Flask(__name__)
 
+service_name = "ecostreet_core_service"
+service_ip = "34.159.194.58:5000"
+
+database_core_service = "34.96.72.77"
+configuration_core_service = "192.168.1.121"
+
 @app.route("/")
 def hello_world():
     return "Login microservice."
@@ -11,6 +17,29 @@ def hello_world():
 @app.route('/login', methods = ['POST'])
 def login():
     login_data = request.form
-    url = 'http://34.96.72.77/authenticate'
+    url = 'http://' + database_core_service + '/authenticate'
     response = requests.post(url, data=login_data)
     return response.text
+
+@app.route("/update_ip")
+def update_ip():
+    data = {"name": service_name, "ip": service_ip}
+    url = 'http://' + configuration_core_service + '/update'
+    response = requests.post(url, data=data)
+    return response.text
+
+@app.route("/config", methods = ['POST'])
+def config_update():
+    try:
+        microservice = request.form["name"]
+        ms_ip = request.form["ip"]
+        if microservice == "database_core_service":
+            database_core_service = ms_ip
+        if microservice == "configuration_core_service":
+            configuration_core_service = ms_ip
+    except Exception as err:
+        return err
+
+@app.route("/getconfig")
+def get_config():
+    return [database_core_service, configuration_core_service]
