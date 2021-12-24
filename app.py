ee829bd22d04227d71681bc2f9df6232ddc7fe31
MatejTheLes <matej.leskovsek.ce@gmail.com>
2021-12-24 15:45:33 +0100

    login update

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def game_command():
         except:
             response = "200 OK"
         return {"response": "You have successfully joined the game."}, 200
-    return {"response": "UNAUTHORIZED"}, 401
+    return {"response": "Game doesn\'t exist."}, 401
 docs.register(game_command)
 
 # SERVICE IP UPDATE FUNCTION
